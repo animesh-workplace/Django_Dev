7b384991ef0f7fca8fadbd343bfb1f031120f49a
animesh <animesh.workplace@gmail.com>
2020-01-04 16:51:02 +0530

    added test page

--- apps/funding_theory/urls.py
+++ apps/funding_theory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index_view'),    
+    path('test', views.test_view, name='test_view'),
 ]
--- apps/funding_theory/views.py
+++ apps/funding_theory/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index_view(request, *args, **kwargs):
 	return render(request, "funding_theory/index.html", {})
+
+def test_view(request, *args, **kwargs):
+	return render(request, "funding_theory/test.html", {})
