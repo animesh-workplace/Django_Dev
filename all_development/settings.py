5f90eafb7293d85d305920d27544849468ab8229
animesh <animesh.workplace@gmail.com>
2020-01-08 15:55:09 +0530

    changed database to sqlite3

--- all_development/settings.py
+++ all_development/settings.py
@@ -89,14 +89,10 @@ WSGI_APPLICATION = 'all_development.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE'    : 'django.db.backends.postgresql',
-        'NAME'      : 'development',
-        'USER'      : 'postgres',
-        'PASSWORD'  : 'Dibya#123',
-        'HOST'      : 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
